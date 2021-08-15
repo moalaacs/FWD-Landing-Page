bed6cdbe6d6610dabf12f3ce0d00c48445e296c5
moalaacs <moalaacs@gmail.com>
2021-08-15 17:43:46 +0200

    Fix active class bug

--- js/app.js
+++ js/app.js
@@ -34,49 +34,56 @@ const myUl = document.getElementById("navbar__list");
 
 // build the nav fucnction
 function navBuilder(){
-    // Loop on all sections to set the correct number of links that will be added.
     allSections.forEach((section)=> {
-        // Create <a> and <li> elements.
         let aAttribute = document.createElement("a");
         let newLink = document.createElement("li");
-        // Set <a> text exactly like section attribute 'data nav'.
         aAttribute.textContent = section.getAttribute("data-nav");
-        // Build and set <a> attribute 'linkToSec' exactly like section id.
-        aAttribute.setAttribute('linkToSec', section.getAttribute("id"));
-        // <li> append <a>.
+        aAttribute.setAttribute('linktosec', section.getAttribute("id"));
         newLink.appendChild(aAttribute);
-        // <ul> append <li>.
         myUl.appendChild(newLink);
     })
+    let activeLi = document.querySelector("a");
+    activeLi.setAttribute('class',"your-active-li");
 }
 
-// Add class 'active' to section when near top of viewport
-
-// Check if the target section is near to of viewport ?
+// Check if the target section is near top of viewport ?
 function viewportChecker(targetSection) {
     let sectionView = targetSection.getBoundingClientRect();
-    // Check if the section position on screen is >= 0 from top ?
-    if(sectionView.top >= 0){
+    if(sectionView.top >= -50 && sectionView.top < 394){
         return true;
+    } else {
+        return false;
     }
 }
 
 // Set active class to target section.
 function setActiveClass() {
-    // Pass all sections one by one to viewPortChecker function.
     for (section of allSections){
-        // Check if sectionView.top >= 0 ?
         if (viewportChecker(section) == true){
-            /* Check if section is already have our class then remove it.
-               if not ? then add our class to this section. */
-            if (section.classList.contains('your-active-class')) {
-                section.classList.remove('your-active-class');
+            if (!section.classList.contains('your-active-class')) {
+                var activeSec = document.getElementsByClassName('your-active-class')[0];
+                activeSec.classList.remove('your-active-class')
+                section.classList.add('your-active-class'); 
             }
-            else {
-                section.classList.add('your-active-class');
             }
         }     
-    } 
+    }
+
+
+// Set active class to target li.
+function setActiveLi() {
+    const allLinks = document.querySelectorAll("a");
+    var activeSection = document.getElementsByClassName('your-active-class')[0];
+    console.log(activeSection);
+        allLinks.forEach((link)=> {
+            if (link.getAttribute("linktosec") == activeSection.getAttribute("id")) {
+                if (!link.classList.contains('your-active-li')) {
+                    var activeLi = document.getElementsByClassName('your-active-li')[0];
+                    activeLi.classList.remove('your-active-li')
+                    link.classList.add('your-active-li'); 
+                }
+            }
+    })      
 }
 
 /**
@@ -89,18 +96,14 @@ function setActiveClass() {
 navBuilder();
 
 // Scroll to section on link click
-
-// Collect all links in our page.
 const allLinks = document.querySelectorAll("a");
 allLinks.forEach((link)=>{
-    // Add eventListener 'click' to every link.
     link.addEventListener("click", ()=>{
-        // Pass link attribute 'linkToSec' to target section id.
         let targetSection = document.getElementById(link.getAttribute("linkToSec"));
-        // Scroll to target section smoothly.
         targetSection.scrollIntoView({behavior:"smooth"});
     })
 })
 
-// Set sections as active
+// Set sections & li as active
 document.addEventListener('scroll', setActiveClass);
+document.addEventListener('scroll', setActiveLi);
